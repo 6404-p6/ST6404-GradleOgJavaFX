0cab6c345e38c6f1bb68417445c65f693eddfbf8
TDuncker <tdunck15@student.aau.dk>
2019-04-30 10:40:31 +0200

    Små ændringer og påbegyndelse af choosePatientView

--- src/main/java/ST6404/choosePatientController.java
+++ src/main/java/ST6404/choosePatientController.java
@@ -1,28 +1,6 @@
-package Patient;
+package ST6404;
 
 public class choosePatientController {
-    private patientModel model;
-    private PatientView view;
-
-    public choosePatientController(patientModel model, PatientView view){
-        this.setModel(model);
-        this.setView(view);
-    }
-
-    public patientModel getModel() {
-        return model;
-    }
-
-    public void setModel(patientModel model) {
-        this.model = model;
-    }
-
-    public PatientView getView() {
-        return view;
-    }
-
-    public void setView(PatientView view) {
-        this.view = view;
-    }
+    
 
 }
