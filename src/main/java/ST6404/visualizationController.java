21945491a9f88006feb30fdbbbb985f63a389885
Tobias <tobiasd@hotmail.dk>
2019-05-01 19:31:18 +0200

    Tilføjelse: View skift (Visualisering -> Medicinliste)

--- src/main/java/ST6404/visualizationController.java
+++ src/main/java/ST6404/visualizationController.java
@@ -1,4 +1,27 @@
-package medicineInteraction;
+package ST6404;
+
+import ST6404.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class visualizationController {
+    @FXML
+    public void changeSceneToMedicineListView(ActionEvent event) throws IOException {
+        System.out.println("Troubleshoot: Begynder metode changeSceneToMedicineListView");
+        // Forklaring følger, men den er her indtil videre: https://www.youtube.com/watch?v=XCgcQTQCfJQ
+        Parent medicineListView = FXMLLoader.load(Main.class.getResource("/medicineListView.fxml"));
+        Scene medicineListViewScene = new Scene(medicineListView);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(medicineListViewScene);
+        window.show();
+        System.out.println("Troubleshoot: Afslutter metode changeSceneToMedicineListView");
+
+    }
 }
