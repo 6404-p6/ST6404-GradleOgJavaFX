6436041e2edf8c3b4a3f62877b6be44a243724e0
TDuncker <tdunck15@student.aau.dk>
2019-05-01 11:23:51 +0200

    Tilføjelse af troubleshoot metode og fix af viewmetode

--- src/main/java/ST6404/medicineListController.java
+++ src/main/java/ST6404/medicineListController.java
@@ -16,14 +16,17 @@ public class medicineListController {
 
     @FXML
     public void changeSceneToMedicineList(ActionEvent event) throws IOException {
+        System.out.println("Troubleshoot: Begynder metode changeSceneToMedicineList");
+        //// Her findes informationerne til medicinlistens view fra database
+
+        //// Her ændres view'et til medicinlisten
         // Forklaring følger, men den er her indtil videre: https://www.youtube.com/watch?v=XCgcQTQCfJQ
         Parent medicineListView = FXMLLoader.load(Main.class.getResource("/medicineListView.fxml"));
         Scene medicineListViewScene = new Scene(medicineListView);
-
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-
         window.setScene(medicineListViewScene);
         window.show();
+        System.out.println("Troubleshoot: Afslutter metode changeSceneToMedicineList");
 
 /* Kunne ikke få det til at virke, men vent til efter søndag d. 5 med at slette dette udkommenteret
         Parent medicineListViewScene = FXMLLoader.load(Main.class.getResource("/medicineListView.fxml"));
