c2be2ff2d74968d56f4a9a88b5098306d938c17e
Sine Andersen <sinand16@student.aau.dk>
2019-05-09 09:45:52 +0200

    opdateret
    opdateret

--- src/main/java/ST6404/medicineCardController.java
+++ src/main/java/ST6404/medicineCardController.java
@@ -64,6 +64,7 @@ public class medicineCardController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("Initialiser medicineListView");
+        databaseConnectorController.interactionList();
         patientModel chosenPatient = patientModel.getInstance();
         IDTitledPaneMedicineList.setText(chosenPatient.getPatientIdentification());
     }
