30cdbe29e9d29017f5963e1ea80bdc7511614635
MarianneGS <msteen15@student.aau.dk>
2019-05-14 16:15:00 +0200

    Tilføjelse til view.fxml

--- src/main/java/ST6404/visualizationController.java
+++ src/main/java/ST6404/visualizationController.java
@@ -7,8 +7,11 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.net.URL;
@@ -20,8 +23,39 @@ import java.util.ResourceBundle;
 
 public class visualizationController implements Initializable {
 
-    @FXML
-    private TitledPane IDTitledPaneVisualization;
+    @FXML private TitledPane IDTitledPaneVisualization;
+
+    @FXML public TextField IDtextfieldDrug2Bottom;
+    @FXML public TextField IDtextfieldDrug3Bottom;
+    @FXML public TextField IDtextfieldDrug4Bottom;
+    @FXML public TextField IDtextfieldDrug5Bottom;
+    @FXML public TextField IDtextfieldDrug6Bottom;
+    @FXML public TextField IDtextfieldDrug7Bottom;
+    @FXML public TextField IDtextfieldDrug8Bottom;
+    @FXML public TextField IDtextfieldDrug9Bottom;
+    @FXML public TextField IDtextfieldDrug10Bottom;
+    @FXML public TextField IDtextfieldDrug11Bottom;
+    @FXML public TextField IDtextfieldDrug12Bottom;
+    @FXML public TextField IDtextfieldDrug13Bottom;
+    @FXML public TextField IDtextfieldDrug14Bottom;
+    @FXML public TextField IDtextfieldDrug15Bottom;
+    @FXML public TextField IDtextfieldDrug16Bottom;
+
+    @FXML public TextField IDtextfieldDrug1Right;
+    @FXML public TextField IDtextfieldDrug2Right;
+    @FXML public TextField IDtextfieldDrug3Right;
+    @FXML public TextField IDtextfieldDrug4Right;
+    @FXML public TextField IDtextfieldDrug5Right;
+    @FXML public TextField IDtextfieldDrug6Right;
+    @FXML public TextField IDtextfieldDrug7Right;
+    @FXML public TextField IDtextfieldDrug8Right;
+    @FXML public TextField IDtextfieldDrug9Right;
+    @FXML public TextField IDtextfieldDrug10Right;
+    @FXML public TextField IDtextfieldDrug11Right;
+    @FXML public TextField IDtextfieldDrug12Right;
+    @FXML public TextField IDtextfieldDrug13Right;
+    @FXML public TextField IDtextfieldDrug14Right;
+    @FXML public TextField IDtextfieldDrug15Right;
 
     // Se forklaring i patientSelector.changeSceneToMedicineListView
     @FXML
@@ -34,7 +68,6 @@ public class visualizationController implements Initializable {
         window.setScene(medicineListViewScene);
         window.show();
         System.out.println("Troubleshoot: Afslutter metode changeSceneToMedicineListView");
-
     }
 
     @Override
@@ -45,5 +78,9 @@ public class visualizationController implements Initializable {
         // Topbjælken får indsat navn og CPR fra metoden getPatientIdentification
         IDTitledPaneVisualization.setText(dataStorage.chosenPatient.getPatientIdentification());
     }
+
+    //@Override
+    //public void Visualization()
+
 
 }
