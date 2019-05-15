947744993bbbd0820c6df2ba0e5c0f48d6256793
Tobias <tobiasd@hotmail.dk>
2019-05-16 01:47:22 +0200

    Tilføjelse af navne på visualiseringen (Klar til næste fase :D!)

--- src/main/java/ST6404/visualizationController.java
+++ src/main/java/ST6404/visualizationController.java
@@ -20,48 +20,47 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class visualizationController implements Initializable {
 
 
-    @FXML public static Text IDtextfieldDrug2Bottom;
-    @FXML public static Text IDtextfieldDrug3Bottom;
-    /*@FXML public TextField IDtextfieldDrug4Bottom;
-    @FXML public TextField IDtextfieldDrug5Bottom;
-    @FXML public TextField IDtextfieldDrug6Bottom;
-    @FXML public TextField IDtextfieldDrug7Bottom;
-    @FXML public TextField IDtextfieldDrug8Bottom;
-    @FXML public TextField IDtextfieldDrug9Bottom;
-    @FXML public TextField IDtextfieldDrug10Bottom;
-    @FXML public TextField IDtextfieldDrug11Bottom;
-    @FXML public TextField IDtextfieldDrug12Bottom;
-    @FXML public TextField IDtextfieldDrug13Bottom;
-    @FXML public TextField IDtextfieldDrug14Bottom;
-    @FXML public TextField IDtextfieldDrug15Bottom;
-    @FXML public TextField IDtextfieldDrug16Bottom;*/
+    @FXML public Text IDtextfieldDrug2Bottom;
+    @FXML public Text IDtextfieldDrug3Bottom;
+    @FXML public Text IDtextfieldDrug4Bottom;
+    @FXML public Text IDtextfieldDrug5Bottom;
+    @FXML public Text IDtextfieldDrug6Bottom;
+    @FXML public Text IDtextfieldDrug7Bottom;
+    @FXML public Text IDtextfieldDrug8Bottom;
+    @FXML public Text IDtextfieldDrug9Bottom;
+    @FXML public Text IDtextfieldDrug10Bottom;
+    @FXML public Text IDtextfieldDrug11Bottom;
+    @FXML public Text IDtextfieldDrug12Bottom;
+    @FXML public Text IDtextfieldDrug13Bottom;
+    @FXML public Text IDtextfieldDrug14Bottom;
+    @FXML public Text IDtextfieldDrug15Bottom;
+    @FXML public Text IDtextfieldDrug16Bottom;
 
-    //@FXML public static TextField IDtextfieldDrug1Right;
-    /*@FXML public TextField IDtextfieldDrug3Right;
-    @FXML public TextField IDtextfieldDrug4Right;
-    @FXML public TextField IDtextfieldDrug5Right;
-    @FXML public TextField IDtextfieldDrug6Right;
-    @FXML public TextField IDtextfieldDrug7Right;
-    @FXML public TextField IDtextfieldDrug8Right;
-    @FXML public TextField IDtextfieldDrug9Right;
-    @FXML public TextField IDtextfieldDrug10Right;
-    @FXML public TextField IDtextfieldDrug11Right;
-    @FXML public TextField IDtextfieldDrug12Right;
-    @FXML public TextField IDtextfieldDrug13Right;
-    @FXML public TextField IDtextfieldDrug14Right;
-    @FXML public TextField IDtextfieldDrug15Right;*/
+    @FXML public Text IDtextfieldDrug1Right;
+    @FXML public Text IDtextfieldDrug2Right;
+    @FXML public Text IDtextfieldDrug3Right;
+    @FXML public Text IDtextfieldDrug4Right;
+    @FXML public Text IDtextfieldDrug5Right;
+    @FXML public Text IDtextfieldDrug6Right;
+    @FXML public Text IDtextfieldDrug7Right;
+    @FXML public Text IDtextfieldDrug8Right;
+    @FXML public Text IDtextfieldDrug9Right;
+    @FXML public Text IDtextfieldDrug10Right;
+    @FXML public Text IDtextfieldDrug11Right;
+    @FXML public Text IDtextfieldDrug12Right;
+    @FXML public Text IDtextfieldDrug13Right;
+    @FXML public Text IDtextfieldDrug14Right;
+    @FXML public Text IDtextfieldDrug15Right;
 
     @FXML
     private TitledPane IDTitledPaneVisualization;
-
-    public static void AddMedication(){
-        IDtextfieldDrug2Bottom.setText("Hej");
-    }
 
     // Se forklaring i patientSelector.changeSceneToMedicineListView
     @FXML
@@ -75,20 +74,51 @@ public class visualizationController implements Initializable {
         window.show();
         System.out.println("Troubleshoot: Afslutter metode changeSceneToMedicineListView");
     }
-    //IDTextfieldDrug1Right
-    /*
 
     private void inputMedicationNames(List medicineCard) {
 
-        List bottomMedicationNames = null;
-        String tempSQLDrugNames = "";
-
-        for(int i = 0; i < medicineList.size(); i++ ){
-            prescriptedDrugModel tempPrecriptedDrugModel = (prescriptedDRugModel) medicineCard.get(i);
-            tempPrescripted
+        List<Text> tempIDTextRightList = new ArrayList<Text>();
+        tempIDTextRightList.add(IDtextfieldDrug1Right);
+        tempIDTextRightList.add(IDtextfieldDrug2Right);
+        tempIDTextRightList.add(IDtextfieldDrug3Right);
+        tempIDTextRightList.add(IDtextfieldDrug4Right);
+        tempIDTextRightList.add(IDtextfieldDrug5Right);
+        tempIDTextRightList.add(IDtextfieldDrug6Right);
+        tempIDTextRightList.add(IDtextfieldDrug7Right);
+        tempIDTextRightList.add(IDtextfieldDrug8Right);
+        tempIDTextRightList.add(IDtextfieldDrug9Right);
+        tempIDTextRightList.add(IDtextfieldDrug10Right);
+        tempIDTextRightList.add(IDtextfieldDrug11Right);
+        tempIDTextRightList.add(IDtextfieldDrug12Right);
+        tempIDTextRightList.add(IDtextfieldDrug13Right);
+        tempIDTextRightList.add(IDtextfieldDrug14Right);
+        tempIDTextRightList.add(IDtextfieldDrug15Right);
+        for(int i = 0; i < medicineCard.size()-1; i++ ) {
+            prescriptedDrugModel tempPrescriptedDrugModelForRightDrugNames = (prescriptedDrugModel) medicineCard.get(i);
+            tempIDTextRightList.get(i).setText(tempPrescriptedDrugModelForRightDrugNames.getMedicationName());
+        }
+            List<Text> tempIDTextBottomList = new ArrayList<Text>();
+        tempIDTextBottomList.add(IDtextfieldDrug2Bottom);
+        tempIDTextBottomList.add(IDtextfieldDrug3Bottom);
+        tempIDTextBottomList.add(IDtextfieldDrug4Bottom);
+        tempIDTextBottomList.add(IDtextfieldDrug5Bottom);
+        tempIDTextBottomList.add(IDtextfieldDrug6Bottom);
+        tempIDTextBottomList.add(IDtextfieldDrug7Bottom);
+        tempIDTextBottomList.add(IDtextfieldDrug8Bottom);
+        tempIDTextBottomList.add(IDtextfieldDrug9Bottom);
+        tempIDTextBottomList.add(IDtextfieldDrug10Bottom);
+        tempIDTextBottomList.add(IDtextfieldDrug11Bottom);
+        tempIDTextBottomList.add(IDtextfieldDrug12Bottom);
+        tempIDTextBottomList.add(IDtextfieldDrug13Bottom);
+        tempIDTextBottomList.add(IDtextfieldDrug14Bottom);
+        tempIDTextBottomList.add(IDtextfieldDrug15Bottom);
+        tempIDTextBottomList.add(IDtextfieldDrug16Bottom);
+            for(int i = 1; i < medicineCard.size(); i++ ){
+                prescriptedDrugModel tempPrescriptedDrugModelForBottomDrugNames = (prescriptedDrugModel) medicineCard.get(i);
+                tempIDTextBottomList.get(i).setText(tempPrescriptedDrugModelForBottomDrugNames.getMedicationName());
         }
     }
-*/
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -102,6 +132,9 @@ public class visualizationController implements Initializable {
         // Instantiering og load af interaktionslisten
         interactionSummarizerModel iSM = new interactionSummarizerModel();
         iSM.setInteractionList(db.loadInteractionsList(dataStorage.chosenPatient.medicineCard.medicineList));
+        IDtextfieldDrug2Right.setText("test af to string ids");
+        // Indsættelse af navne omkring heatmap
+        inputMedicationNames(dataStorage.chosenPatient.medicineCard.medicineList);
     }
 
 }
